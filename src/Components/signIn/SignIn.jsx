f2a4f1c9e86bc1e0e39f5a750d1f6ed6f63d098f
Yash Gupta <yashguptayashu2003@gmail.com>
2024-11-23 22:49:28 +0530

    rm tailwind

---
+++ src/Components/signIn/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 style={{ color: "white", background: "black" }}>test</h1>
+    </div>
+  );
+};
+
+export default SignIn;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from './Components/signIn/SignIn';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SignIn/>
   </React.StrictMode>
 );
 
