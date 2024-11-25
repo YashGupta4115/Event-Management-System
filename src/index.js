06a1f7ef1173d7865128e664c6e3c690f35bb745
Vishal9798 <vishal.p12344@gmail.com>
2024-11-25 20:39:04 +0530

    Checking

--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import LoginPage from '../src/Components/signIn/LoginPage';
-import CreateAccount from '../src/Components/signIn/createAccount';
-
 import reportWebVitals from './reportWebVitals';
 import AdminDashboard from './Routes/AdminDashboard/AdminDashboard';
 import { ScreenContextProvider } from './Contexts/screenContext';
-import CreateAccountPage from './Components/signIn/createAccount';
+import CreateAccountPage from './Components/signIn/createAccountPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ScreenContextProvider>
-    {/* <AdminDashboard/> */}
+    <AdminDashboard/>
     {/* // <LoginPage/> */}
-    <createAccount/>
+    {/* <CreateAccountPage/> */}
   </ScreenContextProvider>
 );
 reportWebVitals();
