edf17d981458e3cfb3b8e4c929a44d2137385329
Vishal9798 <vishal.p12344@gmail.com>
2024-11-25 18:48:06 +0530

    createAccountpage created

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AdminDashboard from './Routes/AdminDashboard/AdminDashboard';
-
+import CreateAccountPage from './Components/signIn/createAccount';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AdminDashboard/>
+    {/* <AdminDashboard/> */}
+    <CreateAccountPage/>    
   </React.StrictMode>
 );
 reportWebVitals();
