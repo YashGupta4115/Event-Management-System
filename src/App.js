5f7d0b1c9c501bf35da6e9de406eb5a082928c1b
archi <archisinha20@gmail.com>
2024-11-28 11:06:45 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
+    
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -21,5 +25,36 @@ function App() {
     </div>
   );
 }
+  
+export default App;*/
+
+
+
+import React, { useState } from "react";
+import './App.css';
+import App1 from "./User/App1";
+import App2 from "./User/App2";
+import App3 from "./User/App3"; // Import App3
+
+function App() {
+  const [activeApp, setActiveApp] = useState(1);
+
+  return (
+    <>
+      {/* Navigation Bar */}
+      <nav className="nav-bar">
+        <button onClick={() => setActiveApp(1)}>Explorer</button>
+        <button onClick={() => setActiveApp(2)}>View Design 2</button>
+        <button onClick={() => setActiveApp(3)}>View Design 3</button>
+      </nav>
+
+      {/* Conditional Rendering */}
+      {activeApp === 1 && <App1 />}
+      {activeApp === 2 && <App2 />}
+      {activeApp === 3 && <App3 />}
+    </>
+  );
+}
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
 import SignIn from './Components/signIn/SignIn';
+import App from './App';
+import App1 from './User/App1';
+import App2 from './User/App2';
+import App3 from './User/App3';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App/>,
+  },
+  {
+    path: "app1",
+    element:<App1/>,
+  },
+  {
+    path: "app2",
+    element:<App2/>,
+  },
+  {
+    path: "app3",
+    element:<App3/>,
+  },
+]);
+
+
+
+//import reportWebVitals from './reportWebVitals';
+//import SignIn from './Components/signIn/SignIn';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SignIn/>
   </React.StrictMode>
+  
+);
+root.render(
+  <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
