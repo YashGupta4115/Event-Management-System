d694ba1ceff5e65137f7a3f24e568dbb3ddb70f7
Vishal9798 <vishal.p12344@gmail.com>
2024-11-30 12:11:02 +0530

    SignIn and Login

--- src/Routes/Home/AppHome.jsx
+++ src/Routes/Home/AppHome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "../../Components/Navbar/Navbar";
 import App1 from "../../User/App1";
 
@@ -13,9 +13,16 @@ import CheckOutPage from "../../Components/CheckOut/CheckOutPage";
 import CreateEvent from "../AdminRoutes/CreateEvent/CreateEvent";
 
 const AppHome = () => {
+  const location = useLocation();
+
+  // List of paths where the Navbar should not appear
+  const hideNavbarPaths = ["/login", "/sign-up"];
+
   return (
     <div className="appHome-container">
-      <Navbar />
+      {/* Conditionally render Navbar */}
+      {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
+
       <Routes>
         <Route path="/" element={<App1 />} />
         <Route path="/events" element={<App2 />} />
