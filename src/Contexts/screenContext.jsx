7cacb0d4b64e82a8a05ca5230e40530d921e78d8
Yash Gupta <yashguptayashu2003@gmail.com>
2024-12-14 18:41:25 +0530

    fix navbar bugs

--- src/Contexts/screenContext.jsx
+++ src/Contexts/screenContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 export const screenContext = createContext();
 
 export const ScreenContextProvider = ({ children }) => {
-  const [width, setWidth] = useState(0);
+  const [width, setWidth] = useState(1024);
   const [isNavOpen, setIsNavBarOpen] = useState(false);
 
   const toggleNav = () => {
